ea97d5b4f0f133ec010f21dde27c10da72f495ae
amandaLuana <amandaluana008@gmail.com>
2020-11-27 21:33:25 -0300

    .

--- exercícios/fundamentos/operadoresUnarios.js
+++ exercícios/fundamentos/operadoresUnarios.js
@@ -2,3 +2,9 @@ let num1 = 1
 let num2 = 2
 
 num1++
+console.log(num1)
+--num1  // operador incremento e decremento pode ser escrito antes ou depois
+console.log(num1)
+
+console.log(++num1 === num2--) // de uma forma pré-fixada ele faz o incremento antes da comparação
+console.log(num1 === num2)
