270d168f4918574b654c4a838f3bb06b5828ff94
amandaLuana <amandaluana008@gmail.com>
2020-11-30 19:30:51 -0300

    Aula 63

---
+++ exercícios/controle/forIn.js
@@ -0,0 +1,19 @@
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+
+// navegando em arrays
+for(let i in notas) {
+    console.log(i, notas[i])
+}
+
+// navegando em objetos
+const pessoa = {
+    nome: 'Ana',
+    sobrenome: 'Silva',
+    idade: 29,
+    peso: 64
+}
+
+for(let atributo in pessoa) {
+    console.log(`${atributo} = ${pessoa[atributo]}`)
+}
+
