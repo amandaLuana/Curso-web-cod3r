944057b526aa9cbd654eb01c8891b0d1e2f4ae46
amandaLuana <amandaluana008@gmail.com>
2020-12-04 23:06:39 -0300

    IIFE

---
+++ exercícios/funcao/iife.js
@@ -0,0 +1,6 @@
+// IIFE -> Immediately invoked function expression
+
+(function () {
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente!')
+})() // não precisa chamar a função, executa automaticamente
