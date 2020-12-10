f4be37c7bc7df619ba813285a3b874ab5ef48363
amandaLuana <amandaluana008@gmail.com>
2020-12-10 19:10:00 -0300

    objeto

---
+++ exercícios/objeto/oo.js
@@ -0,0 +1,17 @@
+// Código não executável
+
+// procedural
+processamento(valor1, valor2, valor3)
+
+// oo você tem dados e dentro do dados tem funções
+objeto = { 
+    valor1,
+    valor2, 
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // foco passou a ser o objeto
+
